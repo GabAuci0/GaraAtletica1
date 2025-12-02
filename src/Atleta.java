7c1c07c2ec7243f44f9b1788dc19ed663bc4a076
GabAuci0 <gabrielepg2007@gmail.com>
2025-12-02 14:45:59 +0100

    commit eventi anomali

--- src/Atleta.java
+++ src/Atleta.java
@@ -1,6 +1,4 @@
-
 import java.util.Random;
-
 
 public class Atleta implements Runnable {
     int numero;
@@ -9,31 +7,59 @@ public class Atleta implements Runnable {
     double metri = 0;
     final double LUNGHEZZAGARA = 50.0;
 
-
     public Atleta(int pNumero, String pNome) {
         numero = pNumero;
         nome = pNome;
         Giudice.aggiungiAtleta(this);
     }
 
-
     @Override
     public void run() {
-        long inizio = System.currentTimeMillis(); //"cronometro"
-        Random metriPercorsi = new Random();
-        while(metri <= LUNGHEZZAGARA) {
-            metri += metriPercorsi.nextDouble(10);
-            System.out.println(nome + " Metri Percorsi: " + metri);
-            try { Thread.currentThread().sleep(1000); }
-            catch (InterruptedException e) { System.err.println("Errore sleep"); }
+        long inizio = System.currentTimeMillis();
+        Random rnd = new Random();
+
+        while (metri <= LUNGHEZZAGARA) {
+
+
+            int evento = rnd.nextInt(100); // 0 - 99
+
+            if (evento < 5) {
+                // 5% - SCIVOLATA
+                System.out.println(nome + " è SCIVOLATO e perde tempo!");
+                sleep(1500);
+            } else if (evento < 10) {
+                // 5% - CRAMPI / RALLENTAMENTO
+                System.out.println(nome + " ha un crampo! Avanza pochissimo...");
+                metri += rnd.nextDouble(1);
+                sleep(1000);
+            } else if (evento < 13) {
+                // 3% - SUPER SPRINT
+                System.out.println(nome + " fa un SUPER SPRINT!!");
+                metri += rnd.nextDouble(20) + 5;
+            } else if (evento < 18) {
+                // 5% - DISTRAZIONE
+                System.out.println(nome + " si distrae e rallenta...");
+                sleep(2000);
+            } else {
+
+                metri += rnd.nextDouble(10);
+            }
+
+            System.out.println(nome + " Metri percorsi: " + metri);
+            sleep(1000);
         }
 
-        long fine = System.currentTimeMillis(); //"stop cronometro"
-
+        long fine = System.currentTimeMillis();
         tempo = (fine - inizio) / 1000.0;
 
         Giudice.finito(this);
     }
+
+    private void sleep(int ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e) {
+            System.err.println("Errore sleep");
+        }
+    }
 }
-
-
